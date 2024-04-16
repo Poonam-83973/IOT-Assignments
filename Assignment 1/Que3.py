f73749576404ef04f490475f842c13150c328fcf
poonamkokitkar <poonamkokitkar19@gmail.com>
2024-04-16 21:48:46 +0530

    3rd and 4th Que

---
+++ Assignment 1/Que3.py
@@ -0,0 +1,8 @@
+# Que3] Write a program to accept three integer numbers and find its average.
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+num3 = int(input("Enter 3rd number: "))
+
+avg = (num1 + num2 + num3)/3
+
+print(f"The average of {num1},{num2} and {num3} is {avg}")
